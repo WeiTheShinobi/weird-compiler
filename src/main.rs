498799d4d7985312211c53e09cf0cb0c6c215a07
WeiTheShinobi <weitheshinobi@gmail.com>
2024-07-13 16:31:30 +0800

    hello, main()!

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env::args;
+use lalrpop_util::lalrpop_mod;
+use std::io::Result;
+use std::fs::read_to_string;
+
+lalrpop_mod!(sysy);
+
+fn main() -> Result<()> {
+    let mut args = args();
+    args.next();
+
+    let mode = args.next().unwrap();
+    let input = args.next().unwrap();
+    args.next();
+    let output = args.next().unwrap();
+
+    let input = read_to_string(input)?;
+    let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+
+    println!("{}", ast);
+    Ok(())
+}
